7e1875f246b2bc1e1b35ea18518d3cedb0f0b2ea
pei_lu <pei_lu@gzhc365.com>
2021-10-14 15:14:45 +0800

    init

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,15 @@
+import React, { Component, useEffect, useState } from "react";
+import { View, Text } from "@tarojs/components";
+import "./index.less";
+
+interface Home {
+  name: string;
+
+}
+const Home: React.FC<Home> = (props) => {
+  const { name } = props;
+
+  return name;
+};
+
+export default Home;
