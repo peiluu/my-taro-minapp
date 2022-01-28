2361f9b5372961687bb0901243f9060c7bf6c202
pei_lu <pei_lu@haici.com>
2022-01-28 16:54:16 +0800

    no message

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ class App extends Component {
 
   // this.props.children 是将要会渲染的页面
   render() {
-    console.log(111111111);
+    console.log('第一次提交');
     console.log(222222222);
     return this.props.children
   }
